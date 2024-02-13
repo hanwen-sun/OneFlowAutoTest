ffb3c7142c492fa92841f8e182e95b4cb0b2329d
shw <shw20010329@163.com>
2024-02-13 05:41:47 +0000

    add prepare.sh for llama

---
+++ llama/prepare.sh
@@ -0,0 +1,7 @@
+# 准备数据
+mkdir llama_dataset && cd llama_dataset
+
+wget https://oneflow-dataset.oss-cn-beijing.aliyuncs.com/libai/loss_compara_content_sentence.bin &&
+wget https://oneflow-dataset.oss-cn-beijing.aliyuncs.com/libai/loss_compara_content_sentence.idx
+
+# 代码替换
